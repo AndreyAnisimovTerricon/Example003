5ad279774071d2462e2a483fe0d1136e40ca71b4
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-08-23 20:32:23 +0300

    программа поиска четных чисел

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int n;
+ n = a % 2;
+ if (n == 0)
+Console.WriteLine("Четное");
+else
+{ Console.WriteLine("Нечетное");
+
+}
